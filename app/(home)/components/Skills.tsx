531b681f422b35b872b89c3066a72701f1389db2
Mostafa Taman <mostafa.taman.me@gmail.com>
2024-02-02 17:27:03 +0200

    style: restyle the skills section & add project cards

--- app/(home)/components/Skills.tsx
+++ app/(home)/components/Skills.tsx
@@ -1,57 +1,78 @@
 import { Brush, Camera, Pencil, Video } from "lucide-react";
 
+import { Button } from "@/components/ui/button";
 import GradientText from "@/components/general/GradientText";
 
 const mySkills = [
     {
         name: "Video Editing",
         icon: <Video className="text-pink-600" size={40} />,
-        desc: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur aliquam doloribus nesciunt eos fugiat. Vitae aperiam fugit consequuntur saepe laborum."
+        desc: "I can edit videos on a professional level using tools like Davinci Reslove and Adobe Premiere."
     },
     {
         name: "Video Creation",
         icon: <Camera className="text-pink-600" size={40} />,
-        desc: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur aliquam doloribus nesciunt eos fugiat. Vitae aperiam fugit consequuntur saepe laborum."
+        desc: "I can high quality video about any topic for social media."
     },
     {
         name: "Script Writing",
         icon: <Pencil className="text-pink-600" size={40} />,
-        desc: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur aliquam doloribus nesciunt eos fugiat. Vitae aperiam fugit consequuntur saepe laborum."
+        desc: "If you need someone to write a script, you've come to the right place."
     },
     {
         name: "Graphic Design",
         icon: <Brush className="text-pink-600" size={40} />,
-        desc: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur aliquam doloribus nesciunt eos fugiat. Vitae aperiam fugit consequuntur saepe laborum."
+        desc: "In this day and age graphics are an essential tool that I know how to weild."
     },
 ];
 
 const Skills: React.FC = () => {
     return (
-        <section className="">
-            <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-32">
+        <section>
+            <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
                 <h2 className="text-3xl font-bold tracki text-center sm:text-5xl dark:text-gray-50">
                     <GradientText text="Skills" />
                 </h2>
 
-                <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-2">
-                    {
-                        mySkills.map((skill) => <div
-                            key={skill.name}
-                            className="block rounded-xl border bg-gray-300 dark:bg-gray-800 cursor-pointer border-gray-800 p-8 shadow-lg transition hover:border-pink-500/10 hover:shadow-pink-500"
+                <div className="grid grid-cols-1 my-8 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16">
+                    <div className="mx-auto max-w-lg text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right">
+                        <h2 className="text-3xl font-bold sm:text-4xl">
+                            Explore some of my skills
+                        </h2>
+
+                        <p className="mt-4 text-gray-600">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vero aliquid sint distinctio
+                            iure ipsum cupiditate? Quis, odit assumenda? Deleniti quasi inventore, libero reiciendis
+                            minima aliquid tempora. Obcaecati, autem.
+                        </p>
+
+                        <Button
+
+                            className="mt-8 inline-block rounded gradient-primary bg-gradient-to-l hover:scale-110 px-12 py-3 text-sm font-medium text-white transition"
                         >
-                            {skill.icon}
+                            Download My Resume
+                        </Button>
+                    </div>
 
-                            <h2 className="mt-4 text-xl font-bold">{skill.name}</h2>
+                    <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-2">
 
-                            <p className="mt-1 text-sm dark:text-gray-300">
+                        {mySkills.map((skill) => <div
+                            key={skill.name}
+                            className="block group rounded-xl border p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
+                        >
+                            <span className="inline-block rounded-lg p-3">
+                                {skill.icon}
+                            </span>
+
+                            <h2 className="mt-2 font-bold">{skill.name}</h2>
+
+                            <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
                                 {skill.desc}
                             </p>
                         </div>
-                        )
-                    }
+                        )}
+                    </div>
                 </div>
-
-
             </div>
         </section>
     );
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import ContactCard from "./components/ContactCard";
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 const Home: React.FC = () => {
@@ -9,6 +10,7 @@ const Home: React.FC = () => {
       <Hero />
       <Skills />
       <About />
+      <Projects />
       <ContactCard />
     </>
   );
