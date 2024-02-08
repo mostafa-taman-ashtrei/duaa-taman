abd9fea9ad916b155ebaa7b12bc09bf4e2706b96
Mostafa Taman <mostafa.taman.me@gmail.com>
2024-02-08 15:01:12 +0200

    feat(video): create a video section synced with cloudinary

--- app/(home)/components/Projects.tsx
+++ app/(home)/components/Projects.tsx
@@ -1,26 +1,31 @@
 import { Brush, Camera, Pencil, Video } from "lucide-react";
 
 import GradientText from "@/components/general/GradientText";
+import Link from "next/link";
 
-const mySkills = [
+const projectCategories = [
     {
         name: "Video Editing",
         icon: <Video className="text-pink-600" size={40} />,
+        url: "/videos",
         desc: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur aliquam doloribus nesciunt eos fugiat. Vitae aperiam fugit consequuntur saepe laborum."
     },
     {
         name: "Video Creation",
         icon: <Camera className="text-pink-600" size={40} />,
+        url: "/videos",
         desc: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur aliquam doloribus nesciunt eos fugiat. Vitae aperiam fugit consequuntur saepe laborum."
     },
     {
         name: "Script Writing",
         icon: <Pencil className="text-pink-600" size={40} />,
+        url: "/videos",
         desc: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur aliquam doloribus nesciunt eos fugiat. Vitae aperiam fugit consequuntur saepe laborum."
     },
     {
         name: "Graphic Design",
         icon: <Brush className="text-pink-600" size={40} />,
+        url: "/videos",
         desc: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur aliquam doloribus nesciunt eos fugiat. Vitae aperiam fugit consequuntur saepe laborum."
     },
 ];
@@ -38,17 +43,19 @@ const Projects: React.FC = () => {
 
                 <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-2">
                     {
-                        mySkills.map((skill) => <div
-                            key={skill.name}
-                            className="block rounded-xl border bg-gray-300 dark:bg-gray-800 cursor-pointer border-gray-800 p-8 shadow-lg transition hover:border-pink-500/10 hover:shadow-pink-500"
-                        >
-                            {skill.icon}
-                            <h2 className="mt-4 text-xl font-bold">{skill.name}</h2>
+                        projectCategories.map((projectCategory) => <Link key={projectCategory.name} href={projectCategory.url}>
+                            <div
+                                key={projectCategory.name}
+                                className="block rounded-xl border bg-gray-300 dark:bg-gray-800 cursor-pointer border-gray-800 p-8 shadow-lg transition hover:border-pink-500/10 hover:shadow-pink-500"
+                            >
+                                {projectCategory.icon}
+                                <h2 className="mt-4 text-xl font-bold">{projectCategory.name}</h2>
 
-                            <p className="mt-1 text-sm dark:text-gray-300">
-                                {skill.desc}
-                            </p>
-                        </div>
+                                <p className="mt-1 text-sm dark:text-gray-300">
+                                    {projectCategory.desc}
+                                </p>
+                            </div>
+                        </Link>
                         )
                     }
                 </div>
---
+++ app/videos/page.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import {
+    CloudinaryContext,
+    Video,
+} from "cloudinary-react";
+import { Ghost, ShieldX } from "lucide-react";
+
+import GradientText from "@/components/general/GradientText";
+import GridLoader from "react-spinners/GridLoader";
+import { VideoType } from "@/types/ui";
+import useSWR from "swr";
+
+const fetcher = async (url: string) => {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error("Failed to fetch data");
+    }
+    return response.json();
+};
+
+const ProjectPage: React.FC = () => {
+    const { data, error, isLoading } = useSWR("/api/videos", fetcher);
+
+    if (isLoading) return <div className="flex justify-center h-screen items-center">
+        <GridLoader color="#7e22ce" className="justify-center" size={30} />
+    </div>;
+
+    if (error) return <div className="mx-auto max-w-screen-xl px-4 py-8 h-screen sm:px-6 sm:py-12 lg:py-9">
+        <p className="text-neutral-500 flex flex-col gap-8 items-center justify-center h-4/5 text-xl">
+            <ShieldX size={80} />
+            We encountered technical issues 😅 ... try again in a minute.
+        </p>
+    </div>;
+
+    if (data === undefined) return <div className="mx-auto max-w-screen-xl px-4 py-8 h-screen sm:px-6 sm:py-12 lg:py-9">
+        <p className="text-neutral-500 flex flex-col gap-3  p-8 h-screen items-center justify-center">
+            <Ghost size={50} />
+            No projects in this category yet 😅 ... Come back later I always update my work.
+        </p>
+    </div>;
+
+
+    return (
+        <section>
+            <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+                <h2 className="text-3xl font-bold tracki text-center sm:text-5xl dark:text-gray-50">
+                    <GradientText text="Video Projects" />
+                </h2>
+                <p className="text-center text-gray-600">
+                    Explore some of my projects. Choose a category and let the magic beign
+                </p>
+
+                <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-2">
+                    {
+                        data !== undefined && data.resources.map((video: VideoType) => (
+                            <div
+                                key={video.public_id}
+                                className="w-100 h-100"
+                            >
+                                <CloudinaryContext cloud_name="codedog">
+                                    <Video
+                                        publicId={video.public_id}
+                                        cloudName={process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}
+                                        secure="true"
+                                        controls
+                                        fallbackContent="Your browser does not support HTML5 video tags."
+                                        format="webm"
+                                    />
+                                </CloudinaryContext>
+                            </div>
+                        ))}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default ProjectPage;
